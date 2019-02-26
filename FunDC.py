e23f8bed297b5491d47237b4d2576bc08691e8ea
DavGP206 <39192183+DavGP206@users.noreply.github.com>
2019-02-26 18:11:54 +0200

    Update FunDC.py

--- FunDC.py
+++ FunDC.py
@@ -1,36 +1,133 @@
+import aiohttp
 import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
 import asyncio
-import time
+import random
+from discord.ext import commands
+from discord.ext.commands import Bot
+import datetime, time
+import os
+import youtube_dl
+from discord.voice_client import VoiceClient
+global playing
+playing = "Jelenleg nem megy semmilyen zene!"
+players = {}
+global chat_filter
+global bypass_list
+bot_prefix= "!!"
+client = commands.Bot(command_prefix=bot_prefix)
+Client = discord.Client()
+vc_clients = {}
+version = 2.3
 
-Client = discord.Client ()
-client = commands.Bot (command_prefix = "!")
+players = {}
+queues = {}
+
+def check_queue(id):
+    if queues[id] != []:
+        players = queues[id].pop(0)
+        players[id] = players
+        players.start()
 
 @client.event
 async def on_ready():
-    print ("Egy bot készen áll. Az Online-ra kapcsolás sikeres volt. Bot ID: 000001")
+    print("Bot online!")
 
 @client.event
 async def on_message(message):
-    if message.content == "!teszt":
-        await client.send_message(message.channel, "Gratulálok! A bot működik, vagyis életre keltettél ^^")
-    if message.content == "!suti":
-        await client.send_message(message.channel, ":cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie:")
-    if message.content.upper().startswith('!MOND'):
-        args = message.content.split(" ")
-        #args[0] = !SAY
-        #args[1] = Hey
-        #args[2] = There
-        #args[1:] = Hey There
-        await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
-    if message.content == "bottt":
-        await client.send_message(message.channel, "Hogy mertél megszólítani? Volt merszed? Szeretlek <3")
-    if message.content == "!rex":
-        await client.send_message(message.channel, "```Szeretem Rex-et :D Adok neki kutyakaját!``` :cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie::cookie:")
-    
-    
+    if message.content.upper().startswith("!!KEX"):
+        await client.send_message(message.channel, ":cookie:")
+
+    if message.content.upper().startswith("!!PING"):
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+
+    if message.content.upper().startswith("!!FIGYELMEZTETES"):
+        if message.author.server_permissions.administrator == True:
+            args = message.content.split(" ")
+            await client.send_message(message.channel, ":warning: Figyelem :warning:")
+            await client.send_message(message.channel, "⠀")
+            await client.send_message(message.channel, ":exclamation: %s" % (" ".join(args[1:])))
+            await client.send_message(message.channel, "⠀")
+            await client.send_message(message.channel, "[ @here ]")
+        else:
+            emb3 = (discord.Embed(Title="Cim", colour=0xff0000))
+            emb3.add_field(name="Ehhez nincs jogod! :(", value="LaserBot")
+            await client.send_message(message.channel, embed=emb3)
+
+    if message.content.upper().startswith("!!DOBOKOCKA"):
+        await client.send_message(message.channel, random.choice([":game_die: Ennyit dobtál: 1",
+                                                                 ":game_die: Ennyit dobtál: 2",
+                                                                 ":game_die: Ennyit dobtál: 3",
+                                                                 ":game_die: Ennyit dobtál: 4",
+                                                                 ":game_die: Ennyit dobtál: 5",
+                                                                 ":game_die: Ennyit dobtál: 6"]))
+
+    if message.content.upper().startswith("!!HELP"):
+        emb = (discord.Embed(Title="Cim", colour=0x3DF270))
+        emb.add_field(name="Parancsok listája", value="⠀")
+        emb.add_field(name="!!help", value="Kiírja a parancsok listáját", inline=False)
+        emb.add_field(name="!!kex", value="Ad egy kexet", inline=False)
+        emb.add_field(name="!!dobokocka", value="Mond egy számot 1-6 között", inline=False)
+        emb.add_field(name="!!ping", value="Pong!", inline=False)
+        emb.set_footer(text="LaserBot")
+        await client.send_message(message.channel, embed=emb)
+
+    if message.content.upper().startswith("!!HELP"):
+        emb2 = (discord.Embed(Title="Cim", colour=0xff0000))
+        emb2.add_field(name="Admin parancsok", value="⠀")
+        emb2.add_field(name="!!figyelmeztetes [Szöveg]", value="Kiírja a pmegadott szöveget Figyelmeztetés ként", inline=False)
+        emb2.set_footer(text="LaserBot")
+        await client.send_message(message.channel, embed=emb2)
+
+@client.command(pass_context=True)
+async def join(ctx):
+        channel = ctx.message.author.voice.voice_channel
+        await client.join_voice_channel(channel)
+        await client.say("Beléptem a hangcsatornába, indíthatjátok a zenéket!")
+
+@client.command(pass_context=True)
+async def left(ctx):
+        server = ctx.message.server
+        voice_client = client.voice_client_in(server)
+        await voice_client.disconnect()
+        await client.say("Elhagytam a hangcsatornát, remélem tetszettek a zenék!")   
+
+@client.command(pass_context=True)
+async def pause(ctx):
+        id = ctx.message.server.id
+        players[id].pause()
+        await client.say("A zenét megállítottam!")
 
 
-        
-client.run ("NDQyMjY5MTcxODU4OTMxNzIz.Dc8Wzg.npmxmpv-eBcuRYKa-5cdqDh9Y8I")
+@client.command(pass_context=True)
+async def stop(ctx):
+    try:
+        id = ctx.message.server.id
+        players[id].stop()
+        await client.say("A zenét leállítottam!")
+    except:
+        await client.say("Jelenleg nem szól egy zene sem!")
+    return False
+
+
+@client.command(pass_context=True)
+async def folytat(ctx):
+    try:
+        id = ctx.message.server.id
+        players[id].resume()
+        await client.say("A zene folytatódik!")
+    except:
+        await client.say("Jelenleg nem szól egy zene sem!")
+    return False
+
+
+@client.command(pass_context=True)
+async def skip(ctx):
+    try:
+        id = ctx.message.server.id
+        players[id].stop()
+    except:
+        await client.say("A zenét átugrottam!")
+    return False                        
+
+client.run("NDQyMjY5MTcxODU4OTMxNzIz.D1QXtA.Nv2FX8GoJPbLSX_EuJksP2n43Iw")
