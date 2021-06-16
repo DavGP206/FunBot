01af2cda25b4d1a4952a13dae7059ee422997d6d
DavGP206 <39192183+DavGP206@users.noreply.github.com>
2021-06-17 00:17:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,33 +4,12 @@ import asyncio
 import random
 from discord.ext import commands
 from discord.ext.commands import Bot
-import datetime, time
-import os
-import youtube_dl
-import hashlib
-import os
-from discord.voice_client import VoiceClient
-global playing
-playing = "Jelenleg nem megy semmilyen zene!"
-players = {}
-global chat_filter
-global bypass_list
 bot_prefix= "!!"
 client = commands.Bot(command_prefix=bot_prefix)
 Client = discord.Client()
 client.remove_command('help')
-vc_clients = {}
 version = 7.9
  
-players = {}
-queues = {}
- 
-def check_queue(id):
-    if queues[id] != []:
-        players = queues[id].pop(0)
-        players[id] = players
-        players.start()
-      
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name="Here to help!"))
